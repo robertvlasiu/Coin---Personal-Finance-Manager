750ac1bfff1b40b376a27694ebbaefe908dff30d
Robert Vlasiu <slimkode@gmail.com>
2019-09-01 00:09:16 +0300

    added bootstrap, started implementing first page

---
+++ application/views/dependencies.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <link rel="stylesheet" href="vendor/twitter/bootstrap/dist/css/bootstrap.min.css">
+    
+    <script src="vendor/components/jquery/jquery.min.js"></script>
+
+    <script src="vendor/twitter/bootstrap/dist/js/bootstrap.min.js"></script>
---
+++ application/views/index.php
@@ -0,0 +1,24 @@
+    <link rel="stylesheet" href="assets/css/homepage.css">
+
+    <meta charset="utf-8">
+	<title>Coin - Personal Finance Manager</title>
+</head>
+<body>
+<div class="bgImageContainer">
+</div>
+<div id="container">
+    <div class="row customCard">
+        <div class="col"></div>
+        <div class="col">
+            <div class="card bg-dark text-white">
+                <div class="card-header"><h1 align="center">Coin</h1><hr></div>
+                <div class="card-body">Basic card</div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    
+</div>
+
+</body>
+</html>
